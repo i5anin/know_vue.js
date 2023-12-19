4577a98b98016260dcc43b1a27ca71997818219c
Isanin Sergey <sergey.isanin@gmail.com>
2023-12-19 23:13:17 +0300

    +

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <v-container>
     <h1>Props</h1>
-    <!-- Блок для отображения подсвеченного кода -->
-    <pre><code class="language-html">{{ childComponentCode }}</code></pre>
-    <pre><code class="language-html">{{ parentComponentCode }}</code></pre>
+    <!-- Блоки для отображения отформатированного и подсвеченного кода -->
+    <pre><code class="language-html">{{ formattedChildComponentCode }}</code></pre>
+    <pre><code class="language-html">{{ formattedParentComponentCode }}</code></pre>
   </v-container>
 </template>
 
 <script>
 import Prism from 'prismjs'
+import Prettier from 'prettier/standalone'
+import parserHtml from 'prettier/parser-html'
+import parserBabel from 'prettier/parser-babel'
 import childComponentCode from '@/views/source/childComponentCode.js'
 import parentComponentCode from '@/views/source/parentComponentCode.js'
 import 'prismjs/themes/prism-tomorrow.css'
@@ -17,12 +20,30 @@ export default {
   name: 'Home',
   data() {
     return {
-      childComponentCode,
-      parentComponentCode,
+      formattedChildComponentCode: '',
+      formattedParentComponentCode: '',
     }
   },
-  mounted() {
-    Prism.highlightAll()
+  async mounted() {
+    this.formattedChildComponentCode = await this.formatCode(childComponentCode)
+    this.formattedParentComponentCode =
+      await this.formatCode(parentComponentCode)
+    this.$nextTick(() => {
+      Prism.highlightAll()
+    })
+  },
+  methods: {
+    async formatCode(code) {
+      try {
+        return await Prettier.format(code, {
+          parser: 'babel', // Используйте 'vue' или 'html' для соответствующего типа кода
+          plugins: [parserBabel, parserHtml],
+        })
+      } catch (error) {
+        console.error('Ошибка форматирования:', error)
+        return code // В случае ошибки возвращаем исходный код
+      }
+    },
   },
 }
 </script>
